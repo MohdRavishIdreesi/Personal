f288218907441ed31a94b577c13748e3f69e73e3
Mohd Ravish <68366654+MohdRavishIdreesi@users.noreply.github.com>
2025-04-26 17:33:06 +0530

    Update aws_commands.sh

--- AWS/aws_commands.sh
+++ AWS/aws_commands.sh
@@ -73,50 +73,53 @@ aws elasticbeanstalk describe-applications
 
 #ALIAS
 
-alias show_AWS_EC2_Instance="aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, State.Name, InstanceType, PublicIpAddress, PrivateIpAddress, KeyName, LaunchTime]' --output table"
-alias show_AWS_EC2_InstanceType="aws ec2 describe-instance-types --query 'InstanceTypes[*].[InstanceType, VCpuInfo.DefaultVCpus, MemoryInfo.SizeInMiB, InstanceStorageSupported]' --output table"
-alias show_AWS_EC2_Volume="aws ec2 describe-volumes --query 'Volumes[*].[VolumeId, Size, VolumeType, State, AvailabilityZone, CreateTime, Encrypted]' --output table"
-alias show_AWS_EC2_Key="aws ec2 describe-key-pairs --query 'KeyPairs[*].[KeyName, KeyPairId, KeyType, CreateTime]' --output table"
-alias show_AWS_EC2_SecurityGroup="aws ec2 describe-security-groups --query 'SecurityGroups[*].[GroupId, GroupName, Description, VpcId]' --output table"
-alias show_AWS_EC2_Image="aws ec2 describe-images --owners self --query 'Images[*].[ImageId, Name, Description, CreationDate, Architecture]' --output table"
-alias show_AWS_EC2_Subnet="aws ec2 describe-subnets --query 'Subnets[*].[SubnetId, VpcId, AvailabilityZone, CidrBlock, State, MapPublicIpOnLaunch]' --output table"
-alias show_AWS_EC2_Vpc="aws ec2 describe-vpcs --query 'Vpcs[*].[VpcId, CidrBlock, IsDefault, State, DhcpOptionsId]' --output table"
-alias show_AWS_EC2_ElasticIP="aws ec2 describe-addresses --query 'Addresses[*].[PublicIp, AllocationId, InstanceId, PrivateIpAddress, Domain]' --output table"
-alias show_AWS_EC2_NetworkInterface="aws ec2 describe-network-interfaces --query 'NetworkInterfaces[*].[NetworkInterfaceId, Status, PrivateIpAddress, SubnetId, VpcId, Attachment.InstanceId]' --output table"
-alias show_AWS_EC2_Region="aws ec2 describe-regions --query 'Regions[*].[RegionName, Endpoint, OptInStatus]' --output table"
-alias show_AWS_EC2_AvalZone="aws ec2 describe-availability-zones --query 'AvailabilityZones[*].[ZoneName, State, RegionName, ZoneId]' --output table"
+alias show_AWS_EC2_Instance="aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceId:InstanceId, StateName:State.Name, InstanceType:InstanceType, PublicIpAddress:PublicIpAddress, PrivateIpAddress:PrivateIpAddress, KeyName:KeyName, LaunchTime:LaunchTime}' --output table"
+alias show_AWS_EC2_InstanceType="aws ec2 describe-instance-types --query 'InstanceTypes[*].{InstanceType:InstanceType, VCpus:VCpuInfo.DefaultVCpus, MemoryInMiB:MemoryInfo.SizeInMiB, InstanceStorageSupported:InstanceStorageSupported}' --output table"
+alias show_AWS_EC2_Volume="aws ec2 describe-volumes --query 'Volumes[*].{VolumeId:VolumeId, Size:Size, VolumeType:VolumeType, State:State, AvailabilityZone:AvailabilityZone, CreateTime:CreateTime, Encrypted:Encrypted}' --output table"
+alias show_AWS_EC2_Key="aws ec2 describe-key-pairs --query 'KeyPairs[*].{KeyName:KeyName, KeyPairId:KeyPairId, KeyType:KeyType, CreateTime:CreateTime}' --output table"
+alias show_AWS_EC2_SecurityGroup="aws ec2 describe-security-groups --query 'SecurityGroups[*].{GroupId:GroupId, GroupName:GroupName, Description:Description, VpcId:VpcId}' --output table"
+alias show_AWS_EC2_Image="aws ec2 describe-images --owners self --query 'Images[*].{ImageId:ImageId, Name:Name, Description:Description, CreationDate:CreationDate, Architecture:Architecture}' --output table"
+alias show_AWS_EC2_Subnet="aws ec2 describe-subnets --query 'Subnets[*].{SubnetId:SubnetId, VpcId:VpcId, AvailabilityZone:AvailabilityZone, CidrBlock:CidrBlock, State:State, MapPublicIpOnLaunch:MapPublicIpOnLaunch}' --output table"
+alias show_AWS_EC2_Vpc="aws ec2 describe-vpcs --query 'Vpcs[*].{VpcId:VpcId, CidrBlock:CidrBlock, IsDefault:IsDefault, State:State, DhcpOptionsId:DhcpOptionsId}' --output table"
+alias show_AWS_EC2_ElasticIP="aws ec2 describe-addresses --query 'Addresses[*].{PublicIp:PublicIp, AllocationId:AllocationId, InstanceId:InstanceId, PrivateIpAddress:PrivateIpAddress, Domain:Domain}' --output table"
+alias show_AWS_EC2_NetworkInterface="aws ec2 describe-network-interfaces --query 'NetworkInterfaces[*].{NetworkInterfaceId:NetworkInterfaceId, Status:Status, PrivateIpAddress:PrivateIpAddress, SubnetId:SubnetId, VpcId:VpcId, AttachedInstanceId:Attachment.InstanceId}' --output table"
+alias show_AWS_EC2_Region="aws ec2 describe-regions --query 'Regions[*].{RegionName:RegionName, Endpoint:Endpoint, OptInStatus:OptInStatus}' --output table"
+alias show_AWS_EC2_AvalZone="aws ec2 describe-availability-zones --query 'AvailabilityZones[*].{ZoneName:ZoneName, State:State, RegionName:RegionName, ZoneId:ZoneId}' --output table"
 
-alias show_AWS_IAM_User="aws iam list-users --query 'Users[*].[UserName,CreateDate]' --output table"
+alias show_AWS_IAM_User="aws iam list-users --query 'Users[*].{UserName:UserName, CreateDate:CreateDate}' --output table"
 
 alias show_AWS_S3="aws s3 ls"
-alias show_AWS_S3_Bucket="aws s3api list-buckets --query 'Buckets[*].[Name, CreationDate]' --output table"
+alias show_AWS_S3_Bucket="aws s3api list-buckets --query 'Buckets[*].{Name:Name, CreationDate:CreationDate}' --output table"
 
-alias show_AWS_CloudWatch_Metric="aws cloudwatch list-metrics --query 'Metrics[*].[Namespace, MetricName, Dimensions]' --output table"
-alias show_AWS_CloudWatch_Alarm="aws cloudwatch describe-alarms --query 'MetricAlarms[*].[AlarmName, StateValue, MetricName, Namespace, Threshold, EvaluationPeriods]' --output table"
+alias show_AWS_CloudWatch_Metric="aws cloudwatch list-metrics --query 'Metrics[*].{Namespace:Namespace, MetricName:MetricName, Dimensions:Dimensions}' --output table"
+alias show_AWS_CloudWatch_Alarm="aws cloudwatch describe-alarms --query 'MetricAlarms[*].{AlarmName:AlarmName, StateValue:StateValue, MetricName:MetricName, Namespace:Namespace, Threshold:Threshold, EvaluationPeriods:EvaluationPeriods}' --output table"
+alias show_AWS_Rds_Instance="aws rds describe-db-instances --query 'DBInstances[*].{DBInstanceIdentifier:DBInstanceIdentifier, DBInstanceClass:DBInstanceClass, Engine:Engine, DBInstanceStatus:DBInstanceStatus, EndpointAddress:Endpoint.Address}' --output table"
+alias show_AWS_Rds_Snapshot="aws rds describe-db-snapshots --query 'DBSnapshots[*].{DBSnapshotIdentifier:DBSnapshotIdentifier, DBInstanceIdentifier:DBInstanceIdentifier, SnapshotType:SnapshotType, Status:Status, SnapshotCreateTime:SnapshotCreateTime}' --output table"
+alias show_AWS_Rds_Cluster="aws rds describe-db-clusters --query 'DBClusters[*].{DBClusterIdentifier:DBClusterIdentifier, Engine:Engine, Status:Status, Endpoint:Endpoint, ClusterCreateTime:ClusterCreateTime}' --output table"
+alias show_AWS_Lamda="aws lambda list-functions --query 'Functions[*].{FunctionName:FunctionName, Runtime:Runtime, LastModified:LastModified, MemorySize:MemorySize, Timeout:Timeout}' --output table"
+alias show_AWS_ECS="aws ecs list-clusters --query 'clusterArns[*]' --output table"
+alias show_AWS_EKS="aws eks list-clusters --query 'clusters[*]' --output table"
 
-alias show_AWS_Rds_Instance="aws rds describe-db-instances --query 'DBInstances[*].[DBInstanceIdentifier, DBInstanceClass, Engine, DBInstanceStatus, Endpoint.Address]' --output table"
-alias show_AWS_Rds_Snapshot="aws rds describe-db-snapshots --query 'DBSnapshots[*].[DBSnapshotIdentifier, DBInstanceIdentifier, SnapshotType, Status, SnapshotCreateTime]' --output table"
-alias show_AWS_Rds_Cluster="aws rds describe-db-clusters --query 'DBClusters[*].[DBClusterIdentifier, Engine, Status, Endpoint, ClusterCreateTime]' --output table"
-
-alias show_AWS_Lamda="aws lambda list-functions --query 'Functions[*].[FunctionName, Runtime, LastModified, MemorySize, Timeout]' --output table"
-
-alias show_AWS_ECS="aws ecs list-clusters --query 'clusterArns' --output table"
-alias show_AWS_EKS="aws eks list-clusters --query 'clusters' --output table"
-
-alias show_AWS_KMS_Key="aws kms list-keys --query 'Keys[*].[KeyId]' --output table"
-alias show_AWS_KMS_Alias="aws kms list-aliases --query 'Aliases[*].[AliasName, TargetKeyId]' --output table"
+alias show_AWS_KMS_Key="aws kms list-keys --query 'Keys[*].{KeyId:KeyId}' --output table"
+alias show_AWS_KMS_Alias="aws kms list-aliases --query 'Aliases[*].{AliasName:AliasName, TargetKeyId:TargetKeyId]' --output table"
 
 alias show_AWS_Cloudformation="aws cloudformation list-stacks --query 'StackSummaries[*].[StackName, StackStatus, CreationTime]' --output table"
 alias show_AWS_Cloudformation_Stack="aws cloudformation describe-stacks --query 'Stacks[*].[StackName, StackStatus, CreationTime, Description]' --output table"
-
 alias show_AWS_Route53_Zone="aws route53 list-hosted-zones --query 'HostedZones[*].[Id, Name, CallerReference, ResourceRecordSetCount]' --output table"
-
 alias show_AWS_SNS_Topic="aws sns list-topics --query 'Topics[*].TopicArn' --output table"
 alias show_AWS_SNS_Subscription="aws sns list-subscriptions --query 'Subscriptions[*].[SubscriptionArn, TopicArn, Protocol, Endpoint]' --output table"
 alias show_AWS_SQS_Queue="aws sqs list-queues --query 'QueueUrls' --output table"
-
 alias show_AWS_ElasticBeanTalk_ENV="aws elasticbeanstalk describe-environments --query 'Environments[*].[EnvironmentName, EnvironmentId, ApplicationName, Status, Health, VersionLabel]' --output table"
 alias show_AWS_ElasticBeanTalk_APP="aws elasticbeanstalk describe-applications --query 'Applications[*].[ApplicationName, Description, DateCreated]' --output table"
+
+alias show_AWS_Cloudformation="aws cloudformation list-stacks --query 'StackSummaries[*].{StackName:StackName, StackStatus:StackStatus, CreationTime:CreationTime}' --output table"
+alias show_AWS_Cloudformation_Stack="aws cloudformation describe-stacks --query 'Stacks[*].{StackName:StackName, StackStatus:StackStatus, CreationTime:CreationTime, Description:Description}' --output table"
+alias show_AWS_Route53_Zone="aws route53 list-hosted-zones --query 'HostedZones[*].{Id:Id, Name:Name, CallerReference:CallerReference, ResourceRecordSetCount:ResourceRecordSetCount}' --output table"
+alias show_AWS_SNS_Topic="aws sns list-topics --query 'Topics[*].{TopicArn:TopicArn}' --output table"
+alias show_AWS_SNS_Subscription="aws sns list-subscriptions --query 'Subscriptions[*].{SubscriptionArn:SubscriptionArn, TopicArn:TopicArn, Protocol:Protocol, Endpoint:Endpoint}' --output table"
+alias show_AWS_SQS_Queue="aws sqs list-queues --query 'QueueUrls[*]' --output table"
+alias show_AWS_ElasticBeanTalk_ENV="aws elasticbeanstalk describe-environments --query 'Environments[*].{EnvironmentName:EnvironmentName, EnvironmentId:EnvironmentId, ApplicationName:ApplicationName, Status:Status, Health:Health, VersionLabel:VersionLabel}' --output table"
+alias show_AWS_ElasticBeanTalk_APP="aws elasticbeanstalk describe-applications --query 'Applications[*].{ApplicationName:ApplicationName, Description:Description, DateCreated:DateCreated}' --output table"
 
 ################################################################################################
 ################################# AWS COMMANDS CHIT SHEET ######################################
